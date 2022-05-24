a09a16b5ec1190bb937eb272b7bea64494fb6cdc
juliamagallares <julia.magallares@alumnos.upm.es>
2022-05-24 18:47:16 +0200

    gg

--- Proyecto/Trabajo_Informatica_A109/Trabajo_Informatica_A109/Source.cpp
+++ Proyecto/Trabajo_Informatica_A109/Trabajo_Informatica_A109/Source.cpp
@@ -607,3 +607,78 @@ void guardar_en_disco(int v[], int total)
 	fclose(fichero);
 }
 */
+
+
+
+#define MAX 10
+
+void guardar_en_disco(int v[], int temp);
+int leer_desde_disco(int v[]);
+
+int main(void)
+{
+	int temperaturas[MAX];
+	int i, temperatura, total = 0;
+
+	setlocale(LC_ALL, "es-ES");
+	total = leer_desde_disco(temperaturas);
+	printf("Partimos de\n");
+	for (i = 0; i < total; i++)
+		printf("%d ", temperaturas[i]);
+
+	printf("Introduzca enteros (FIN=-1)");
+	scanf_s("%d", &temperatura);
+	while (temperatura != -1)
+	{
+		temperaturas[total] = temperatura;
+		total++;
+		scanf_s("%d", &temperatura);
+	}
+	printf("Ahora hay\n");
+	for (i = 0; i < total; i++)
+		printf("%d ", temperaturas[i]);
+	guardar_en_disco(temperaturas, total);
+
+	return 0;
+}
+
+int leer_desde_disco(int v[])
+{
+	FILE* fichero;
+	errno_t e;
+	int total = 0, temperatura;
+
+	e = fopen_s(&fichero, "Temperaturas.txt", "rt");
+	if (fichero == NULL)
+		printf("Error: No se ha podido abrir el fichero\n");
+	else
+	{
+		fscanf_s(fichero, "%d", &temperatura);
+		while (!feof(fichero))
+		{
+			v[total] = temperatura;
+			total++;
+			fscanf_s(fichero, "%d", &temperatura);
+		}
+		fclose(fichero);
+	}
+	return total;
+}
+
+void guardar_en_disco(int v[], float* temp)
+{
+	FILE* fichero;
+	errno_t e;
+	int i;
+
+	e = fopen_s(&fichero, "Temperaturas.txt", "at");
+	if (fichero == NULL)
+		printf("Error: No se ha podido guardar el fichero\n");
+	else
+	{
+
+		fprintf(fichero, "%.2f", temp);
+
+	}
+	fclose(fichero);
+}
