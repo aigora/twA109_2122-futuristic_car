a987f25ba6500140c98e46c4b9402c987a572a2e
DavidOM23 <100845119+DavidOM23@users.noreply.github.com>
2022-05-26 14:56:11 +0200

    Update arduino.ino

--- Proyecto/arduino/arduino.ino
+++ Proyecto/arduino/arduino.ino
@@ -1,6 +1,5 @@
 const int EchoPin = 8;
 const int TriggerPin = 9; 
-//const int BuzzPin= 10;
 const int pinLDR=A0;
 const int SensorPIN = A1;
 const int pinIN4=11;
@@ -8,10 +7,7 @@ const int pinIN3=12;
 const int pinIN1=10;
 const int pinIN2=7;
 #define led 13
-#include <Servo.h>
-#include <math.h>
-Servo servo;
-int pos = 0; 
+#include <math.h> 
 const int Rc = 10000; //valor de la resistencia
 const int Vcc = 5;
 float A = 1.11492089e-3;
@@ -25,7 +21,6 @@ servo.attach(11);
 Serial.begin(9600);
 pinMode (TriggerPin, OUTPUT); 
 pinMode (EchoPin, INPUT);
-//pinMode (BuzzPin, OUTPUT);
 pinMode(led,OUTPUT);
 while(!Serial) { ; }
 pinMode(pinIN4,OUTPUT);
@@ -35,16 +30,19 @@ pinMode(pinIN1,OUTPUT);
 }
 
 void loop () {
-////**COMUNICACION**////
+ //*ULTRASONIDOS*//
   int cm = ping(TriggerPin, EchoPin);
+ //*LDR*//
   int ilum=analogRead(pinLDR);
-   float raw = analogRead(SensorPIN);
+ //*TERMISTOR(temperatura)*//
+  float raw = analogRead(SensorPIN);
   float V =  raw / 1024 * Vcc;
   float R = (Rc * V ) / (Vcc - V);
   float logR  = log(R);
   float R_th = 1.0 / (A + B * logR + C * logR * logR * logR );
   float kelvin = R_th - V*V/(K * R)*1000;
   float celsius = kelvin - 273.15;
+ ////**COMUNICACION**////
 if( Serial.available()> 0){
    String str = Serial.readStringUntil('\n');
     if (str.compareTo("GET_DISTANCIA")==0)
@@ -56,7 +54,7 @@ if( Serial.available()> 0){
       else
       if(str.compareTo("GET_TEMPERATURA")==0)
       str=celsius;
-      else
+      else//Aquí empieza el control de los motores
       if(str.compareTo("w")==0)
       {digitalWrite(pinIN4,HIGH);
       digitalWrite(pinIN3,LOW);
@@ -96,10 +94,9 @@ if( Serial.available()> 0){
       str="COMANDO DESCONOCIDO";
  
  Serial.println(str);
-   digitalWrite(pinIN3,HIGH); 
-   digitalWrite(pinIN4,LOW);
+   
 }
-
+//Aquí están las pruebas realizadas para cada sensor
   ////**TEMPERATURA**////
   /*float raw = analogRead(SensorPIN);
   float V =  raw / 1024 * Vcc;
@@ -116,27 +113,21 @@ if( Serial.available()> 0){
    
 ////**LDR y LEDS**////
 //int ilum=analogRead(pinLDR); //originalmente esta variable estaba aquí pero la he puesto arriba para que funcione la parte de comunicación
-//Serial.println("Iluminacion:  ");
-//Serial.println(ilum);   /*Serial.println(ilum-350);*/
+//Serial.println("Iluminacion:  ");//para ver en el programa de arduino los datos
+//Serial.println(ilum);  
   delay(500);
-  if(ilum<300){ /*if((ilum-350)<300)*/
+  if(ilum<300){//para el led
     digitalWrite(led,HIGH);
   }
   else{
         digitalWrite(led,LOW);
 
   }
-////**ULTRASON y BUZZ**////
+////**ULTRASON**////
 //int cm = ping(TriggerPin, EchoPin); //originalmente esta variable estaba aquí pero la he puesto arriba para que funcione la parte de comunicación
-//Serial.println("Distancia:  ");
+//Serial.println("Distancia:  ");//para ver en el programa de arduino los datos
 //Serial.println(cm);
 delay(500);
-//if(cm>10){
-  //digitalWrite(BuzzPin,HIGH);
-//}
-//else{
- // digitalWrite(BuzzPin,LOW);
-//}
 
 }
 
@@ -148,7 +139,7 @@ delay(500);
 
 
 
-////Función del ultrason////
+////Función del ultrasonidos////
 int ping(int TriggerPin, int EchoPin) {
   long duration, distanceCm;
 
